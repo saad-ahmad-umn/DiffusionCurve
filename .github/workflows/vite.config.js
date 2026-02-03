836ac2498a81909cf2b32d3cff00301cf78bdcd1
Saad Ahmad <saad.ahmad.ad@gmail.com>
2026-02-02 22:03:59 -0800

    Update vite.config.js

--- .github/workflows/vite.config.js
+++ .github/workflows/vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // Replace 'your-repo-name' with your actual GitHub repository name
 export default defineConfig({
   plugins: [react()],
-  base: '/your-repo-name/',
+  base: '/DiffusionCurve/',
   resolve: {
     alias: {
       '@': '/src',
